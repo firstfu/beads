07da4338311b1bb145c86f75d785f0a94b13c6c9
firstfu <firefirstfu@gmail.com>
2026-02-24 01:31:57 +0800

    項目初始化

---
+++ beadsTests/beadsTests.swift
@@ -0,0 +1,16 @@
+//
+//  beadsTests.swift
+//  beadsTests
+//
+//  Created by firstfu on 2026/2/24.
+//
+
+import Testing
+
+struct beadsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
